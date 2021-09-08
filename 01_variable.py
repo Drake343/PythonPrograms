7989cc15e080424f4bfe613036f73a0a35edd65a
Drake343 <87871563+Drake343@users.noreply.github.com>
2021-09-08 13:43:53 +0530

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,16 @@
+a = '''Drake'''
+b = 345
+c = 45.32
+d = True
+
+# Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ 03_typecasting.py
@@ -0,0 +1,4 @@
+a = "89drak90"
+a = int(a)
+print(type(a))
+print(a + 5)
