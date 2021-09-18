6c752cad14b9d8021b29b40dbf297d2c2bd87a93
Drake <87871563+Drake343@users.noreply.github.com>
2021-09-18 13:50:41 +0530

    Update and rename 09_pr_05.py to searching_if_else.py

---
+++ searching_if_else.py
@@ -0,0 +1,7 @@
+names = ["DRAKE", "BANDHAN", "ROHIT", "ROHAN", "SAMEER", "SHILPA"]
+name = input("Enter the name to check\n")
+
+if name in names:
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
